643b947e22dca68c1f802d4266e92bac219c4529
LiboCorn <106642508+LiboCorn@users.noreply.github.com>
2023-04-02 15:39:01 +0900

    Generate Infected Link

---
+++ linkgenerator.c
@@ -0,0 +1,10 @@
+#include"linkgen.h"
+INFECTEDLINK *il = { 0 };
+WEBHOOK wh = "webhook link here"
+INFECTEDLINK il = GenerateNewLink((CHAR*)GetListOfLinks());
+for (;;) {
+PrintOn(il);
+if!(il == INVALID_LINK) {
+break;
+}
+}
